f7a87a3574a8f4fc5ce68ac886c30e56b16ab9a9
GermanBogatov <Bogat1weee@mail.ru>
2020-02-27 22:34:40 +0300

    Bot Zavedenyavokruge ispravlenie

--- src/main/java/MyAbilityExtension.java
+++ src/main/java/MyAbilityExtension.java
@@ -4,16 +4,18 @@ import org.telegram.abilitybots.api.objects.Locality;
 import org.telegram.abilitybots.api.objects.Privacy;
 import org.telegram.abilitybots.api.sender.SilentSender;
 import org.telegram.abilitybots.api.util.AbilityExtension;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 
 
 public class MyAbilityExtension implements AbilityExtension {
     private SilentSender silent;
-    DBContext db;
+    private DBContext db;
+    private DBManager dbManager;
 
-
-    public MyAbilityExtension(SilentSender silent, DBContext db) {
+    MyAbilityExtension(SilentSender silent, DBContext db) {
         this.silent = silent;
         this.db = db;
+        dbManager = new DBManager(db);
     }
 
     public Ability start() {
@@ -24,9 +26,9 @@ public class MyAbilityExtension implements AbilityExtension {
                 .locality(Locality.ALL)
                 .privacy(Privacy.PUBLIC)
                 .action(ctx -> {
-                    DBManager dbManager = new DBManager(db);
+
                     silent.send("/typefilter - to filter places by three types: restaurant, bar, cafe" +
-                            "\n/cousinfilter - to filter places by cousin: italian, american, japanese, russian"+
+                            "\n/cuisinefilter - to filter places by cousin: italian, american, japanese, russian"+
                             "\n/checkfilter - to filter places by average check", ctx.chatId());
                 })
                 .build();
@@ -41,12 +43,12 @@ public class MyAbilityExtension implements AbilityExtension {
                 .locality(Locality.ALL)
                 .privacy(Privacy.PUBLIC)
                 .action(ctx -> {
-                    DBManager dbManager = new DBManager(db);
+
                     dbManager.searchByType(ctx.firstArg().toUpperCase(), ctx.update().getMessage().getLocation());
                     for (Place place : dbManager.searchByType(ctx.firstArg().toUpperCase(),
                             ctx.update().getMessage().getLocation())) {
                         silent.send(place.getName() +"\n" + place.getDescription() + "\nAverage check: "
-                                + place.getAverageCheck()  + "\nCousin: " +
+                                + place.getAverageCheck()  + "\nCuisine: " +
                                 place.getCousin().toString().toLowerCase() + "\n" + place.getLocation(), ctx.chatId());
                     }
                 })
@@ -56,17 +58,17 @@ public class MyAbilityExtension implements AbilityExtension {
     public Ability filterByCousin() {
         return Ability
                 .builder()
-                .name("cousinfilter")
-                .info("filter places by cousin")
+                .name("cuisinefilter")
+                .info("filter places by cuisine")
                 .input(1)
                 .locality(Locality.ALL)
                 .privacy(Privacy.PUBLIC)
                 .action(ctx -> {
-                    DBManager dbManager = new DBManager(db);
+
                     for (Place place : dbManager.searchByCousin(ctx.firstArg().toUpperCase(),
                             ctx.update().getMessage().getLocation())) {
                         silent.send(place.getName() +"\n" + place.getDescription() + "\nAverage check: "
-                                + place.getAverageCheck() + "\nCousin: " +
+                                + place.getAverageCheck() + "\nnCuisine: " +
                                 place.getCousin().toString().toLowerCase() + "\n" + place.getLocation(), ctx.chatId());
                     }
                 })
@@ -82,39 +84,16 @@ public class MyAbilityExtension implements AbilityExtension {
                 .locality(Locality.ALL)
                 .privacy(Privacy.PUBLIC)
                 .action(ctx -> {
-                    DBManager dbManager = new DBManager(db);
+
                     for (Place place : dbManager.searchByCheck(Integer.parseInt(ctx.firstArg()),
                             ctx.update().getMessage().getLocation())) {
                         silent.send(place.getName() +"\n" + place.getDescription() + "\nAverage check: "
-                                + place.getAverageCheck() + "\nCousin: " +
+                                + place.getAverageCheck() + "\nCuisine: " +
                                 place.getCousin().toString().toLowerCase() , ctx.chatId());
-                        //silent.execute(new SendLocation(ctx.chatId(), place.getLocation().getLongitude(), place.getLocation().getLatitude()));
-                    }
+                        silent.execute(new SendLocation(place.getLocation().getLongitude(), place.getLocation().getLatitude()));
+                         }
                 })
                 .build();
     }
-   /* private void addPlace(PlaceType type) {
-        Reply.of(update -> {
-            String name = "", description = "";
-            //Location location = update.getMessage().getLocation();
-            silent.send("Name:", update.getMessage().getChatId());
-            name = update.getMessage().getText();
-            silent.execute(Keyboard.addKeyboard(new String[]{"description"}, update,
-                    "tap the button below"));
-            switch (update.getMessage().getText()) {
-                case ("description"):
-                    silent.send("Description:", update.getMessage().getChatId());
-                    description = update.getMessage().getText();
-                    silent.execute(Keyboard.addKeyboard(new String[]{"location"}, update,
-                            "tap the button below"));
-                    break;
-                case ("location"):
-                    silent.send("send a location with map", update.getMessage().getChatId());
-                    Place place = new Place(name, type, description, update.getMessage().getLocation());
-                    dbManager.updateDB(place);
-                    break;
-            }
-        }, update -> Arrays.asList(Constants.keyboardWords).contains(update.getMessage().getText()));
-    }*/
 
 }
